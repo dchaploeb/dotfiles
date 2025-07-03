bf287aafa9e5f209853862961ea77464475252c0
David Chaplin-Loebell <davidcl@weaversway.coop>
2025-07-03 13:40:11 -0400

    Update .chezmoidata/packages.yaml Update .chezmoitemplates/Microsoft.PowerShell_profile.ps1 Update AppData/Roaming/wtq/wtq.schema.json

--- .chezmoitemplates/Microsoft.PowerShell_profile.ps1
+++ .chezmoitemplates/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,15 @@
-$profileDir = Join-Path $HOME "bin/pwsh_profile.d"
+if ($null -ne $Host.UI.RawUI) {
+    # Likely interactive
+    $profileDir = Join-Path $HOME "bin/pwsh_profile.d"
 
-if (Test-Path $profileDir) {
-    Get-ChildItem -Path $profileDir -Filter *.ps1 | Sort-Object Name | ForEach-Object {
-        try {
-            Write-Host "Running $($_.Name)..."
-            . $_.FullName
-        } catch {
-            Write-Warning "Failed to load $($_.Name): $_"
+    if (Test-Path $profileDir) {
+        Get-ChildItem -Path $profileDir -Filter *.ps1 | Sort-Object Name | ForEach-Object {
+            try {
+                Write-Host "Running $($_.Name)..."
+                . $_.FullName
+            } catch {
+                Write-Warning "Failed to load $($_.Name): $_"
+            }
         }
     }
 }
